30bf56f5bd99478120409ff15fb364218232994f
Forever Inc dev <redodevs@gmail.com>
2024-01-18 14:57:09 +0300

    save

---
+++ src/components/Modal.tsx
@@ -0,0 +1,78 @@
+import { BsX } from "react-icons/bs";
+import toast from "react-hot-toast";
+
+
+const Modal = ({ func, isOpen }: { func: () => void, isOpen: boolean }) => {
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //const email = e.currentTarget.email.value;
+        toast.success("Thanks for signing up!");
+        e.currentTarget.reset();
+        func();
+    }
+
+
+
+
+  return (
+      <>
+          <div
+              id="default-modal"
+              onClick={func}
+              tabIndex={1}
+              aria-hidden="true"
+              className={`${isOpen ? "flex" : "hidden"} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 bg-gray-800/70 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full`}
+          >
+              <div onClick={(e) => e.stopPropagation()} className="relative p-4 z-[100] animate-fadeIn w-full max-w-2xl max-h-full">
+                  <div className="relative bg-white rounded-lg shadow-lg border border-gray-800  dark:bg-gray-900">
+                      <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                              Welcome Aboard
+                          </h3>
+                          <button
+                              onClick={func}
+                              type="button"
+                              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                              data-modal-hide="default-modal"
+                          >
+                              <BsX className="w-5 h-5"/>
+                              <span className="sr-only">Close modal</span>
+                          </button>
+                      </div>
+
+                      <div className="p-4 md:p-5 space-y-4"><p>
+                          Stay ahead of the curve! Subscribe to our newsletter
+                          and be the first to know when GemGuru launches.
+                          Receive exclusive updates on discounts, promotions,
+                          and exciting new products. Don't miss out on the
+                          latest news – be part of the GemGuru community!</p>
+                          <form className="py-8" onSubmit={handleSubmit}>
+                              <div className="relative">
+                                  <input
+                                      type="email"
+                                      id="email"
+                                      name="email"
+                                      className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                      placeholder=" "
+                                  />
+                                  <label
+                                      htmlFor="floating_outlined"
+                                      className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                                  >
+                                      Email Address
+                                  </label>
+                              </div>
+                              <div className="py-4">
+                                  <button className="py-2 rounded hover:text-gray-600 hover:bg-white transition-all px-5 border border-gray-600 text-white">Subscribe</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </>
+  );
+}
+
+export default Modal
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,42 @@
 import Timer from "../components/Timer";
 import Bg from "../assets/bg.svg";
+import Logo from "../assets/logo.webp"
+import Modal from "../components/Modal";
+
+
+
 import { IoSparklesOutline } from "react-icons/io5";
+import { useState } from "react";
 const HomePage = () => {
+
+
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    }
+
+
+
     return (
         <>
+            <Modal func={toggleModal} isOpen={showModal} />
             <section
                 className="w-full h-screen flex items-center justify-center bg-cover bg-center relative"
                 style={{ backgroundImage: `url(${Bg})` }}
             >
-                <div className="w-full h-full flex flex-col items-center justify-center bg-gray-900/80 text-center">
-                    <h1 className="text-6xl font-bold text-white animate__animated animate__zoomInDown mb-4">
+                <div className="w-full h-full p-4 flex flex-col items-center justify-center bg-gray-900/80 text-center">
+                    <div className="absolute mb-4 top-0 left-0">
+                        <img src={Logo} alt="Logo" className="w-32 h-32 object-cover" />
+                    </div>
+                    <h1 className=" text-3xl md:text-6xl font-bold text-white animate__animated animate__zoomInDown mb-4">
                         Embrace the Brilliance: Welcome to <br />
                         <div className="flex items-center justify-center">
                             <span className="text-[#C0C0C0] relative">
                                 Gem
-                                <IoSparklesOutline className="animate-pulse text-[#D4AF37] absolute left-0 bottom-0 w-4 h-4"/>
+                                <IoSparklesOutline className="animate-pulse text-[#BD9851] absolute left-0 bottom-0 w-4 h-4"/>
                             </span>
-                            <span className="relative text-[#D4AF37]">
+                            <span className="relative text-[#BD9851]">
                                 <IoSparklesOutline className="absolute animate-pulse text-white w-4 h-4 top-2 -right-1" />
                                 Guru
                             </span>
@@ -32,8 +52,8 @@ const HomePage = () => {
                     </div>
                     <div className="animate__animated animate__backInDown animate__delay-2s">
                         <Timer />
-                    
-                    <button className="bg-transparent hover:bg-white text-white hover:text-gray-900 py-2 px-4 border border-white rounded">
+
+                    <button onClick={toggleModal} className="bg-transparent hover:bg-white text-white hover:text-gray-900 py-2 px-4 border border-white rounded">
                         Notify Me When We Open
                         </button>
                         </div>
